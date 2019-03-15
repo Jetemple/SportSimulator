24160ee17a21e3352810dcac59fd08e094af922c
Jack <jetemple@iastate.edu>
2019-03-15 10:18:31 -0500

    Working using rounds only
    this version only works using a round by round format.

--- SportsSimulator/.idea/NCAAB_wholeTourny.py
+++ SportsSimulator/.idea/NCAAB_wholeTourny.py
@@ -5,17 +5,19 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 import urllib3
+import re
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 url = 'https://www.whatifsports.com/ncaab/'
 browser = webdriver.Firefox()
-browser.get(url)
+
 
 #Opens the Teams and Results file for reading and writting
-with open('Teams_64.txt') as t, open('Results_64.txt', 'w') as r:
+with open('Teams_64.txt') as t, open('Results_64.txt', 'w') as r, open('Winners_64.txt', 'w') as w:
     for line in t:
+        browser.get(url)
         #Re-initializing Variables
         homeWins = 0
         awayWins = 0
@@ -23,7 +25,7 @@ with open('Teams_64.txt') as t, open('Results_64.txt', 'w') as r:
         awayLandslide = 0
 
         #Num of Simulations per game
-        sims = 2
+        sims = 10
 
         r.write(line + "\n") #Writes the text in the results
         split = line.split()
@@ -31,10 +33,27 @@ with open('Teams_64.txt') as t, open('Results_64.txt', 'w') as r:
         #To detect headers IE: South, East etc..
         if split.__len__() == 1:
             continue
+        # elif split.__len__() > 1:
+        #     # re.split('&', line)
+        #     home = split[0]
+        #     away = split[1]
+        #     print home
+        #     print away
+
 
         #Finds the home and away team. Doesn't really matter since they are at neutral site
-        home = split[0]
-        away = split[1]
+
+        if split.__len__() == 3:
+            home = split[0]
+            away = split[1] + " " + split[2]
+
+        elif split.__len__() == 4:
+            home = split[0] + " " + split[1]
+            away = split[2] + " " + split[3]
+        else:
+            home = split[0]
+            away = split[1]
+
 
         #Selects the away teams from the dropdown menus (Maybe could make a function out of these)
         ddAway = Select(browser.find_element_by_id("vname"))
@@ -62,6 +81,7 @@ with open('Teams_64.txt') as t, open('Results_64.txt', 'w') as r:
         iframe = browser.find_elements_by_tag_name('iframe')
 
         for i in range(0, sims):
+            time.sleep(3)
             f = browser.find_elements_by_tag_name('iframe')[0]
             browser.switch_to.frame(0)
             awayScore = browser.find_element_by_xpath('/html/body/div[2]/div[1]/table/tbody/tr[2]/td[4]').text
@@ -83,11 +103,11 @@ with open('Teams_64.txt') as t, open('Results_64.txt', 'w') as r:
                     awayLandslide = awayLandslide + 1
 
 
+
             #Cleaning up the browser and refreshing to simulate another game
             browser.refresh()
             alert = browser.switch_to.alert
             alert.accept()
-            time.sleep(2)
             browser.switch_to.default_content()
 
             #EXTRA STUFF FOR TESTING
@@ -103,25 +123,43 @@ with open('Teams_64.txt') as t, open('Results_64.txt', 'w') as r:
 
 
         #Printing result data
-        print(home + ": " + str(homeWins) + " - (by 10+): " + str(homeLandslide))
-        print(away + ": " + str(awayWins) + " - (by 10+): " + str(awayLandslide))
+        #With Dominations
+        # print(home + ": " + str(homeWins) + "\t\t (by 10+): " + str(homeLandslide))
+        # print(away + ": " + str(awayWins) + "\t\t (by 10+): " + str(awayLandslide))
+        print(" ")
+        #Without Dominations
+        print(home + ": " + str(homeWins))
+        print(away + ": " + str(awayWins))
         print(" ")
 
         #Writing result data -> Results
-        r.write(home + ": " + str(homeWins) + " - (by 10+): " + str(homeLandslide) + "\n")
-        r.write(away + ": " + str(awayWins) + " - (by 10+): " + str(awayLandslide) + "\n")
+
+        # With Dominations
+        # r.write(home + ": " + str(homeWins) + "\t (by 10+): " + str(homeLandslide) + "\n")
+        # r.write(away + ": " + str(awayWins) + "\t (by 10+): " + str(awayLandslide) + "\n")
+        #No Dominations
+        r.write(home + ": " + str(homeWins) + "\n")
+        r.write(away + ": " + str(awayWins) + "\n")
         r.write("\n")
 
+        #Determining Winners
+        if homeWins > awayWins:
+            w.write(home + "\n")
+        else:
+            w.write(away + "\n")
+
         #Closes the popup window so next teams can be selected.
-        fancyClose = browser.find_element_by_id('fancybox-close')
-        fancyClose.click()
-        visitClear = browser.find_element_by_id('visitorclearbtn')
-        time.sleep(.5)
-        visitClear.click()
-        homeClear = browser.find_element_by_id('homeclearbtn')
-        time.sleep(.5)
-        homeClear.click()
+        # fancyClose = browser.find_element_by_id('fancybox-close')
+        # fancyClose.click()
+        # time.sleep(1)
+        # visitClear = browser.find_element_by_id('visitorclearbtn')
+        # time.sleep(1)
+        # visitClear.click()
+        # homeClear = browser.find_element_by_id('homeclearbtn')
+        # time.sleep(1)
+        # homeClear.click()
 
 
 t.close()
 r.close()
+w.close()
